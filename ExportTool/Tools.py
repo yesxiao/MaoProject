39a2b073d8c5a49ac306270c245f4d7cefb08dbb
yesxiao <yesxiao@qq.com>
2024-10-27 23:20:05 +0800

    支持最新版英语完形填空

--- ExportTool/Tools.py
+++ ExportTool/Tools.py
@@ -3,28 +3,28 @@ import re
 
 
 def split_options(text):
-    text = text.replace("．",".")
-    arr = ["A","B","C","D","E","F"]
-    text.replace("．",".")
+    text = text.replace("．", ".")
+    arr = ["A", "B", "C", "D", "E", "F"]
+    text.replace("．", ".")
     rtnArr = []
     sIdx = -1
     curDesc = ""
-    isLastLatterIsEmpty:bool = True
+    isLastLatterIsEmpty: bool = True
     for lIdx in range(len(text)):
         curLeter = text[lIdx]
-        if curLeter in arr and lIdx < len(text) - 1 and text[lIdx+1] == "." and isLastLatterIsEmpty and arr.index(curLeter) == sIdx + 1:
+        if curLeter in arr and lIdx < len(text) - 1 and text[lIdx + 1] == "." and isLastLatterIsEmpty and arr.index(
+                curLeter) == sIdx + 1:
             # 开始
-            if curDesc != "" and len(curDesc) > 1 :
+            if curDesc != "" and len(curDesc) > 1:
                 rtnArr.append(curDesc)
             curDesc = "" + curLeter
             sIdx = sIdx + 1
         else:
             curDesc = curDesc + curLeter
         isLastLatterIsEmpty = curLeter == " " or curLeter != "\t" or curLeter != "\n"
-    if len(curDesc) > 1 :
+    if len(curDesc) > 1:
         rtnArr.append(curDesc)
     return rtnArr
-
 
 
 # 替换 (1)为1.
@@ -36,48 +36,51 @@ def replace_parentheses_with_period(text):
 
     return replaced_text
 
-def getNumPairs(fromId:int,content:str,end:str ,max:int ):
-    if fromId <= 0 :
-        print("getNumPairs ID错误，%d" % fromId )
+
+def getNumPairs(fromId: int, content: str, end: str = None, max: int = -1):
+    if fromId <= 0:
+        print("getNumPairs ID错误，%d" % fromId)
         return None
 
-    idx = checkStrNumFormat( fromId , content )
+    idx = checkStrNumFormat(fromId, content)
     if idx == -1:
         return None
     idx = idx + len(str(fromId)) + 1
-    if fromId >= max :
+    if fromId >= max != -1:
         return content[idx:]
     times = 1
-    idx1 = checkStrNumFormat( fromId + 1 , content , times)
+    idx1 = checkStrNumFormat(fromId + 1, content, times)
     # 为了解决解析中也有数字+点，特别是 1.xx4.xx 2.dddd 3.eee 4.xx不会被误识别
-    while idx1 != -1 and idx1 < idx :
+    while idx1 != -1 and idx1 < idx:
         times = times + 1
-        idx1 = checkStrNumFormat( fromId + 1 , content , times)
+        idx1 = checkStrNumFormat(fromId + 1, content, times)
     if idx1 == -1:
         rtnStr = content[idx:]
-        if end and fromId + 1 == max :
+        if end and fromId + 1 == max:
             endIdx = rtnStr.find(end)
             if endIdx != -1:
                 return rtnStr[0:endIdx]
         return rtnStr
     return content[idx:idx1]
 
-def checkStrNumFormat(fromId:int,content:str,newN:int = -1):
+
+def checkStrNumFormat(fromId: int, content: str, newN: int = -1):
     _n = 1
-    if newN > 0 :
+    if newN > 0:
         _n = newN
     lastIdx = -1
-    while True :
-        idx = solve(content,("%d." % fromId),_n)
-        if idx == -1 or ( lastIdx != -1 and  idx > lastIdx ):
+    while True:
+        idx = solve(content, ("%d." % fromId), _n)
+        if idx == -1 or (lastIdx != -1 and idx > lastIdx):
             return lastIdx
-        if idx > 0 :
-            exNumStr = content[idx-1:idx]
+        if idx > 0:
+            exNumStr = content[idx - 1:idx]
             if exNumStr.isdigit():
                 _n += 1
                 continue
         lastIdx = idx
         _n += 1
+
 
 #字符第几次出现
 def solve(s, str, n):
@@ -86,17 +89,19 @@ def solve(s, str, n):
         return -1
     return len(s) - len(sep[-1]) - len(str)
 
+
 def getImportPath():
-    path="导入.txt"
+    path = "导入.txt"
     if not os.path.exists(path):
         raise Exception("导入文件不存在%s" % path)
-    with open(path,"r",encoding="utf-8") as f:
+    with open(path, "r", encoding="utf-8") as f:
         content = f.read()
         content = content.strip()
         return content
 
+
 #获得【数字】开头的部分
-def isReadingMain(content:str):
+def isReadingMain(content: str):
     # 定义正则表达式模式
     pattern = r'^【(\d+)】'
     # 使用 re.match 来查找匹配项
@@ -108,13 +113,60 @@ def isReadingMain(content:str):
         return False
 
 
+def getContentBeforeNumAndPot(content: str):
+    #一定要包含点
+    if content.find(".") == -1:
+        return content
+    # 定义正则表达式模式
+    # 匹配从字符串的开头到第一个数字加点之前的所有内容
+    pattern = r'^(.*?)(?=\d+\.)'
+
+    # 使用 re.search 查找第一个匹配项
+    match = re.search(pattern, content)
+
+    if match:
+        # 返回匹配的内容
+        return match.group(1).strip()
+    else:
+        # 如果没有找到匹配项，则返回 None 或者空字符串
+        return content
+
+
+
+#获得第一个数字+点格式中的数字
+def get_first_number_before_dot(text):
+    # 定义正则表达式模式
+    # 匹配数字加点
+    pattern = r'(\d+)\.'
+
+    # 使用 re.search 查找第一个匹配项
+    match = re.search(pattern, text)
+
+    if match:
+        # 返回匹配的数字
+        return int(match.group(1))
+    else:
+        # 如果没有找到匹配项，则返回 None 或者空字符串
+        return None
+
+
+# 从指定数字开始，最大的数值
+def get_max_number( text: str) :
+    from_num:int = get_first_number_before_dot(text)
+    if from_num is None:
+        return None
+    #最多检查100
+    last_num:int = from_num
+    cur_num:int = from_num
+    for i in range(100):
+        if text.find( str(cur_num) + "." ) == -1:
+            return last_num
+        last_num = cur_num
+        cur_num = cur_num + 1
 
 
 if __name__ == '__main__':
-    test: str = (r"A．Taichung,helGE.lo. PAWS	B．DetrA.oi Ct Pub.lic Radio"
-                 r"．UNICEF.	D．ORBIS.")
-    opts = split_options(test)
-    for k in opts:
-        print(k )
-
-
+    test: str = (r"【导语】本文是一篇说明文。文章说明了在语言学习的中、高级阶段的单词学习法。"
+                 r"31.考查动词词义辨析。句意：然而，功能性33.语言的熟练，需要掌握相当多")
+    opts = get_max_number(test)
+    print(opts)
---
+++ ExportTool/mainEnCloze.py
@@ -0,0 +1,378 @@
+# coding=UTF-8
+import os
+import sys
+import docx
+import re
+from docx import Document
+from enum import Enum
+
+from docx.shared import Pt, RGBColor
+
+from Tools import getNumPairs, getImportPath, split_options, getContentBeforeNumAndPot, get_first_number_before_dot, \
+    get_max_number
+
+
+class State(Enum):
+    invaid = 0  # 无效
+    main = 1  # 题干
+    opt = 3  # 选项
+    answer = 4  # 答案
+    point = 5  # 知识点
+    anly = 6  # 解析或导语
+    finish = 7
+
+
+class BaseItem:
+
+    def __init__(self):
+        self.content = ""
+
+    def addContent(self, add_str: str):
+        self.content = self.content + add_str
+
+    #解析
+    def update(self):
+        return
+
+
+#选项
+class OptionItem(BaseItem):
+
+    def Update(self):
+        pass
+
+
+#知识点
+class KnowledgePointItem(BaseItem):
+    def __init__(self):
+        super().__init__()
+        self.desc = ""
+
+
+#答案
+class AnswerItem(BaseItem):
+    pass
+
+
+#解析
+class AnalysisItem(BaseItem):
+
+    def __init__(self):
+        super().__init__()
+        self.desc = None
+        self.anlyItems = []
+
+    def update(self):
+        super().update()
+        self.desc = getContentBeforeNumAndPot(self.content)
+        first_id: int = get_first_number_before_dot(self.content)
+        if first_id is None:
+            print("解析中没有题号,解析内容:%s" % self.content)
+            return
+        max_id: int = get_max_number(self.content)
+        for i in range(max_id - first_id + 1):
+            anly = getNumPairs(first_id + i, self.content)
+            if anly is None or anly == "":
+                print("解析内容为空,id=%d" % (first_id + i))
+                continue
+            self.anlyItems.append(anly)
+
+
+class MainItem(BaseItem):
+
+    def __init__(self):
+        super().__init__()
+        # 选项数组
+        self.options = []
+        # 知识点
+        self.main_knowledge_point: KnowledgePointItem = None
+        # 答案
+        self.answer = None
+        # 解析
+        self.analysis: AnalysisItem = None
+
+    def update(self):
+        super().update()
+        for o in self.options:
+            o.update()
+        if self.main_knowledge_point:
+            self.main_knowledge_point.update()
+        self.answer.update()
+        if self.analysis :
+            self.analysis.update()
+
+    def add_option(self, opt: OptionItem):
+        self.options.append(opt)
+
+    def add_answer(self, answer_item: AnswerItem):
+        self.answer = answer_item
+
+    def add_knowledge_point(self, item: KnowledgePointItem):
+        self.main_knowledge_point = item
+
+    def set_analysis(self, item: AnalysisItem):
+        self.analysis = item
+
+
+cur_state = State.invaid
+is_valid = False
+question_rule = re.compile(r"^\d+\s*\.\s*")
+optionROle = re.compile(r"^[A-Z]\s*\.\s*")
+mainItems = []
+cur_main_item: MainItem = None
+#当前选项
+cur_opt_item: OptionItem = None
+#答案
+cur_answer_item: AnswerItem = None
+#知识点
+cur_knowledge_point: KnowledgePointItem = None
+#解析
+cur_analysis_item: AnalysisItem = None
+
+
+def main():
+    if len(sys.argv) > 1:
+        fileName = sys.argv[1]
+    else:
+        fileName = getImportPath()
+    if not fileName.endswith(".docx"):  # 文件夹
+        for path in os.listdir(fileName):
+            if path.endswith(".docx"):
+                path = fileName + "/" + path
+                HandleFile(path)
+    else:
+        HandleFile(fileName)
+
+
+def common_repalce(line:str):
+    line = line.replace("【导语】","【解析】")
+    return line
+
+def HandleFile(file_name: str):
+    global cur_state, is_valid, mainItems, cur_main_item, cur_opt_item, cur_answer_item, cur_knowledge_point, cur_analysis_item
+    cur_state = State.invaid
+    is_valid = False
+    mainItems = []
+    cur_main_item = None
+    cur_opt_item = None
+    cur_answer_item = None
+    cur_knowledge_point = None
+    cur_analysis_item = None
+    if file_name.__contains__("[上传]"):
+        return
+    if not os.path.exists(file_name):
+        print("文件不存在%s" % file_name)
+        return
+    doc = getDoc(file_name)
+    print("----------------开始处理文件 %s ------------" % file_name)
+    global category
+    global type
+    name, category, type, mark = AnalyzeFileName(file_name)
+
+    for d in doc.paragraphs:
+        line = d.text.replace("．", ".")
+        if line == '':
+            continue
+        line = common_repalce(line)
+        is_new_item = checkState(line)
+        if cur_state == State.finish:
+            break
+        if cur_state == State.invaid:
+            continue
+        update_state(line, is_new_item)
+
+    for item in mainItems:
+        main_item: MainItem = item
+        main_item.update()
+
+    ## 写答案
+    file_name = file_name.split(".")[0] + "[上传].docx"
+    writeAns(file_name)
+    print("----------------结束处理文件----------------------------\n")
+    return True
+
+
+def checkState(s: str):
+    global cur_state
+    global is_valid
+    is_new_item: bool = True
+    s = s.strip()
+    if s.find("//题目开始") != -1:
+        is_valid = True
+        return is_new_item
+    if s.find("//题目结束") != -1:
+        cur_state = State.finish
+        return is_new_item
+    if not is_valid:
+        return
+    if s.startswith("#"):
+        cur_state = State.main
+        return is_new_item
+    # 只有题干或选项，才有可能是题目
+    if cur_state.value <= State.opt.value and is_option(s):  # 选项
+        cur_state = State.opt
+        return is_new_item
+    if cur_state.value <= State.answer.value and s.find("【答案】") != -1:
+        cur_state = State.answer
+        return is_new_item
+    if cur_state.value <= State.point.value and s.find("【知识点】") != -1:
+        cur_state = State.point
+        return is_new_item
+    if cur_state.value <= State.anly.value and s.find("【解析】") != -1:
+        cur_state = State.anly
+        return is_new_item
+    is_new_item = False
+    return is_new_item
+
+
+def update_state(line: str, is_new: bool):
+    global cur_state
+    global is_valid, cur_main_item, mainItems, cur_opt_item, cur_answer_item, cur_knowledge_point, cur_analysis_item
+
+    if not is_valid:
+        return
+    match cur_state:
+        case State.main:  #主题文章
+            if cur_main_item is None or is_new:
+                cur_main_item = MainItem()
+                mainItems.append(cur_main_item)
+            cur_main_item.addContent(line)
+        case State.opt:  #选项
+            if cur_main_item is None:
+                print("没有 主文章，识别成选项了. 内容: %s" % line)
+                return
+            if cur_opt_item is None or is_new:
+                cur_opt_item = OptionItem()
+                cur_main_item.add_option(cur_opt_item)
+            cur_opt_item.addContent(line)
+        case State.answer:  #答案
+            if is_new:
+                cur_answer_item = AnswerItem()
+                cur_main_item.add_answer(cur_answer_item)
+            cur_answer_item.addContent(line)
+        case State.point:
+            if is_new:
+                cur_knowledge_point = KnowledgePointItem()
+                cur_main_item.add_knowledge_point(cur_knowledge_point)
+            cur_knowledge_point.addContent(line)
+        case State.anly:
+            if is_new:
+                cur_analysis_item = AnalysisItem()
+                cur_main_item.set_analysis(cur_analysis_item)
+            cur_analysis_item.addContent(line)
+
+
+def is_option(line: str):
+    global question_rule
+    if question_rule.match(line):
+        return True
+    return False
+
+
+def AnalyzeFileName(str):
+    pathArr = str.split("/")
+    str = pathArr[len(pathArr) - 1]
+    titleNameArr = str.split(".")
+    if len(titleNameArr) >= 1:
+        str = titleNameArr[0]
+    arr = str.split("_")
+    if len(arr) == 0:
+        return str, "", "", 1
+    name = arr[0]
+    category = ""
+    type = ""
+    mark = 1
+    if len(arr) > 0:
+        category = arr[1]
+    if len(arr) > 1:
+        type = arr[2]
+    return name, category, type, mark
+
+
+def isQustion(str):
+    if len(str) <= 1:
+        return False
+    global question_rule
+    if question_rule.match(str):
+        return True
+    return False
+
+
+def isAns(str):
+    global analysisRule
+    if analysisRule.match(str):
+        return True
+    return False
+
+
+def getDoc(filename):
+    d = docx.Document(filename)
+    return d
+
+
+def writeAns(fileName):
+    global category
+    global type
+    global mainItems
+    doc = Document()  # 创建新文档
+    para1 = doc.add_paragraph()
+    content: str = ""
+    main_id:int = 0
+    allQue: int = 0
+    for main_item in mainItems:
+        pass
+
+
+    run_2 = para1.add_run(content)  # 以add_run的方式追加内容，方便后续格式调整
+    run_2.font.name = 'Times New Roman'  # 注：这个好像设置 run 中的西文字体
+    # 设置中文字体
+    # 需导入 qn 模块
+    from docx.oxml.ns import qn
+    # run_2.font.name = '楷体'  # 注：如果想要设置中文字体，需在前面加上这一句
+    run_2.font.element.rPr.rFonts.set(qn('w:eastAsia'), '楷体')
+    # 设置字体大小
+    run_2.font.size = Pt(14)
+    if os.path.exists(fileName):
+        os.remove(fileName)
+    doc.save(fileName)  # 文档保存
+    print("保存完成,题干数量%d,子题目数量%d" % (len(mainItems), allQue))
+
+
+if __name__ == '__main__':
+    main()
+
+"""
+1.优考试在线考试系统于        上线，是一款安全、稳定、专业的在线考试系统，支持SaaS在线考试和        ，考试全程智能化处理，比如考前        、考中技术支持、考后数据分析，客服全流程跟进服务，保障考试顺利流畅进行。且优考试拥有多达        种防作弊功能，为考试安全保驾护航。哈哈哈哈         OK  。 航。哈哈哈哈        OK 
+1.
+A. 2014
+B.2015 => 2
+C.2016
+D.2017
+2.
+A. 局域网考试 => 2
+B.线下考试
+C.无网络考试
+D. 飞书考试
+3.
+A. 压力测试 => 2
+B.布置作业
+C.培训
+D. 观看视频
+4.
+A. 8
+B.9
+C.13 => 2
+D. 10
+5.
+A. 8
+B.9
+C.13 => 2
+D. 10
+6.
+A. 8
+B.9
+C.13 => 2
+D. 10
+答案：BAACDD
+解析: 术支持、考后数据
+"""
